3d9cc373ccba1ed66eb59543ed13d9acf03c437a
AntoPrakashURK21CS1168 <110010968+AntoPrakashURK21CS1168@users.noreply.github.com>
2022-09-13 12:03:34 +0530

    Anto

--- Main.py
+++ Main.py
@@ -1,17 +1,13 @@
 from typing import List
 
-def insertionSort(array) -> List[int]:
-  # Write your code here
-   for step in range(1, len(array)):
-        key = array[step]
-        j = step - 1     
-        while j >= 0 and key < array[j]:
-            array[j + 1] = array[j]
-            j = j - 1
-        
-        array[j + 1] = key
-   return array
-
+def selectionSort(array, size) -> List[int]:
+  for step in range(size):
+    min_idx = step
+    for i in range(step + 1, size):
+      if array[i] < array[min_idx]:
+        min_idx = i
+    (array[step], array[min_idx]) = (array[min_idx], array[step])
+  return array
 
 input_data = input()
 data = []
@@ -20,4 +16,4 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-print(insertionSort(data)
+print(selectionSort(data, len(data)))
